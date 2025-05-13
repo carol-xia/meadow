d21a0cbbd38e5e87aa99cae57f297a51cb2ffa8a
Carol Xia <c.xia.98@gmail.com>
2025-05-13 10:03:30 -0700

    404 error page

--- src/views/PageNotFound.tsx
+++ src/views/PageNotFound.tsx
@@ -1,9 +1,18 @@
+import { Typography } from '@mui/material';
+import '../styles/pageNotFound.css'
+
 const PageNotFound = () => {
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for could not be found.</p>
-    </>
+    <div className="fullscreen">
+      <div className='content'>
+        <Typography variant="h3" align="center" gutterBottom>
+          404 - Page Not Found
+        </Typography>
+        <Typography variant="h4" align="center" gutterBottom>
+          Sorry, the page you are looking for could not be found.
+        </Typography>
+      </div>
+    </div>
   );
 }
   
