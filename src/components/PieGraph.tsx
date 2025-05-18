f3a058dd334a19d4f3301cb05def266ef5dacd3b
Carol Xia <c.xia.98@gmail.com>
2025-05-18 16:19:10 -0700

    cleanup

--- src/components/PieGraph.tsx
+++ src/components/PieGraph.tsx
@@ -62,8 +62,8 @@ export default function PieGraph() {
           valueFormatter,
         },
       ]}
-      width={400}
-      height={400}
+      // width={400}
+      // height={400}
     />
   );
 }
