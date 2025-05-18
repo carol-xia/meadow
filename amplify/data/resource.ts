e4e917e00f16578910bf900f974ec51ca0799230
Russell Llave <meadow.financial.tracker@gmail.com>
2025-05-18 16:06:52 -0700

    updated data model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,13 +1,19 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 const schema = a.schema({
-  Note: a
+  ExpenseItem: a
     .model({
-      name:a.string(),
-      description: a.string(),
-      image: a.string(),
+      username: a.string().required(),
+      expenseReportName: a.string().required(),
+      purchaseName: a.string().required(),
+      creationDate: a.datetime().required(),
+      category: a.string().required(),
+      purchaseDate: a.datetime(),
+      price: a.float().required(),
     })
-    .authorization((allow) => [allow.owner()]),
+    .authorization((allow) => [allow.owner()])
+    .identifier(["username", "expenseReportName"])
+    .secondaryIndexes((index) => [index("purchaseName")]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
